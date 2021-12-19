1b4323cf3da5cc5585c0d1d4d60510b4c2944f13
Cong Hoang <cong@coderpush.com>
2021-12-20 00:09:18 +0700

    Update

---
+++ 04-use-context-hook/pages/index.js
@@ -0,0 +1,15 @@
+import styles from "../styles/Home.module.css";
+/**
+ * https://reactjs.org/docs/hooks-reference.html#usecontext
+ * 
+ * 
+ * 
+ * 
+ */
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
